587568907a05180ce5e1f44be57dd577a0e8d14f
Alyssandro <dyogo.alyssandro@gmail.com>
2025-05-30 12:13:47 -0300

    fix: correction of application securit about the _blank

--- front-end/src/Components/Whatsapp Icon/WhatsappIcon.js
+++ front-end/src/Components/Whatsapp Icon/WhatsappIcon.js
@@ -3,15 +3,13 @@ import "./WhatsappIcon.css"
 import icon from "../../images/whatasapp logo.jpg"
 
 function WhatasappIcon() {
-
     return (
         <div id="whatsapp-box">
-            <a href="//wa.me/558396500630" target="_blank">
-                <img id="whatsapp" src={icon} ></img>
+            <a href="//wa.me/558396500630" target="_blank" rel="noreferrer">
+                <img id="whatsapp" alt="Imagem logo do WhatsApp" src={icon} />
             </a>
         </div>
     );
-
 }
 
 export default WhatasappIcon;
