0a2a6b4f770e375a6890e00b70ddff0e6df642f7
Alyssandro <dyogo.alyssandro@gmail.com>
2025-06-06 11:48:52 -0300

    fix: remotion of the first version of the validation status of orders

--- back-end/routes/PaymentStatusRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPaymentStatus } = require('../controllers/PaymentStatusController');
-
-router.get('/payment-status', getPaymentStatus);
-
-module.exports = router;
