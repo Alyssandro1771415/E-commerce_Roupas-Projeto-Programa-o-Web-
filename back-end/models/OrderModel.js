659a9932cdde657f0760c5d2aa4f218b99a32f4b
Alyssandro <dyogo.alyssandro@gmail.com>
2025-06-06 11:50:21 -0300

    fix: correction of the OrderModel based in the MySQL databse table

--- back-end/models/OrderModel.js
+++ back-end/models/OrderModel.js
@@ -2,59 +2,42 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const Order = sequelize.define('Order', {
-  total: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-    validate: {
-      min: 0
-    }
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
-  status: {
-    type: DataTypes.ENUM('pendente', 'processando', 'enviado', 'entregue', 'cancelado'),
-    defaultValue: 'pendente'
-  },
-  shippingAddress: {
-    type: DataTypes.STRING,
+  user_id: {
+    type: DataTypes.INTEGER,
     allowNull: false
   },
-  paymentMethod: {
-    type: DataTypes.STRING,
-    allowNull: false
+  order_date: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
   }
 }, {
-  timestamps: true
+  timestamps: false,
+  tableName: 'orders'
 });
 
 const OrderItem = sequelize.define('OrderItem', {
-  name: {
-    type: DataTypes.STRING,
+  order_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
     allowNull: false
   },
   quantity: {
     type: DataTypes.INTEGER,
-    allowNull: false,
-    validate: {
-      min: 1
-    }
-  },
-  price: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-    validate: {
-      min: 0
-    }
+    allowNull: false
   }
 }, {
-  timestamps: false
+  timestamps: false,
+  tableName: 'order_products' // Nome exato da tabela
 });
-
-const User = require('./User');
-const Product = require('./Product');
-
-Order.belongsTo(User, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
-Order.hasMany(OrderItem, { as: 'items', foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
-
-OrderItem.belongsTo(Order, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
-OrderItem.belongsTo(Product, { foreignKey: 'productId', allowNull: false });
 
 module.exports = { Order, OrderItem };
